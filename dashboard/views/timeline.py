2ee351fc35422ce81f4c7f73b5e51304ee8c346f
laffra <laffra@gmail.com>
2023-06-27 18:10:27 +0200

    Draw grid lines

--- dashboard/views/timeline.py
+++ dashboard/views/timeline.py
@@ -2,29 +2,65 @@
 # Microlog. Copyright (c) 2023 laffra, dcharbon. All rights reserved.
 #
 
+import itertools
 from dashboard import canvas
 from dashboard import config
 
 class Timeline():
     def draw(self, canvas: canvas.Canvas):
         self.clear(canvas)
-        tick =  0.01 if canvas.scaleX > 128 else \
-                0.1 if canvas.scaleX > 4 else \
-                1000.0 if canvas.scaleX < 0.00390625 else \
-                100.0 if canvas.scaleX < 0.0625 else \
-                10.0 if canvas.scaleX < 0.5 else \
-                1.0
+        self.drawTicks(canvas)
+    
+    def drawGridLines(self, canvas):
+        lines = []
+        canvas.lines(
+            itertools.chain([
+                ( call.x, call.y + call.h, call.x + call.w, call.y + call.h )
+                for call in calls
+                if canvas.toScreenDimension(call.w) > cls.minWidth and call.threadId in cls.showThreads
+            ]),
+            1,
+            "gray"
+        )
+        canvas.lines(itertools.chain(lines), 1, "gray")
+    
+    def drawTicks(self, canvas):
         y = config.TIMELINE_OFFSET_Y + config.TIMELINE_HEIGHT 
         h = canvas.height()
+        lines = []
+        ticks = []
+        texts = []
+        tick = self.getTick(canvas)
+
+        def addTick(n, x):
+            second = n * tick
+            ticks.append((x, y - 13, x, h))
+            label = f"{second:0.2f}s" if tick < 0.1 else f"{second:0.1f}s" if tick < 1 else f"{second:0.0f}s"
+            cx = x - canvas.fromScreenDimension((len(label) * config.FONT_SIZE_SMALL / 4))
+            texts.append((cx, config.TIMELINE_OFFSET_Y + 23, label, "gray", canvas.fromScreenDimension(100)))
+            lines.append((x, 0, x, config.TIMELINE_OFFSET_Y))
+
+        self.drawVisibleSeconds(canvas, addTick)
+        canvas.lines(ticks, 1, "gray")
+        canvas.lines(lines, 1, "gray")
+        canvas.texts(texts, config.FONT_SMALL)
+
+    def getTick(self, canvas):
+        return 0.01 if canvas.scaleX > 128 else \
+               0.1 if canvas.scaleX > 4 else \
+               1000.0 if canvas.scaleX < 0.00390625 else \
+               100.0 if canvas.scaleX < 0.0625 else \
+               10.0 if canvas.scaleX < 0.5 else \
+               1.0
+
+    def drawVisibleSeconds(self, canvas, draw):
+        tick = self.getTick(canvas)
         for n in range(0, 1000):
             second = n * tick
             x = second * config.PIXELS_PER_SECOND
             if x < 0:
                 continue
-            canvas.line(x, y - 13, x, h, 2, "#BBBA")
-            label = f"{second:0.2f}s" if tick < 0.1 else f"{second:0.1f}s" if tick < 1 else f"{second:0.0f}s"
-            cx = x - canvas.fromScreenDimension((len(label) * config.FONT_SIZE_SMALL / 4))
-            canvas.text(cx, config.TIMELINE_OFFSET_Y + 7, label, "gray", canvas.fromScreenDimension(100), config.FONT_SMALL)
+            draw(n, x)
             if canvas.toScreenX(x) >= canvas.width():
                 break
 
