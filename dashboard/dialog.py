d3f8e11842d54eebcd62366b8ad8595280c59c65
laffra <laffra@gmail.com>
2023-06-28 12:26:37 +0200

    Enhance dialog UX

--- dashboard/dialog.py
+++ dashboard/dialog.py
@@ -3,6 +3,7 @@
 #
 
 import js # type: ignore
+import pyodide # type: ignore
 from typing import List
 
 from dashboard import canvas
@@ -13,25 +14,47 @@ MOUSE_OFFSET = 20
 class Dialog():
     def __init__(self):
         self.showing = False
+        self.dialog = js.jQuery("#dialog")
 
     def show(self, canvas: canvas.Canvas, x:float, y:float, html:str):
-        dialog = js.jQuery("#dialog").css("display", "block").html(html)
-        width = dialog.width()
-        height = dialog.height()
-        screenX = canvas.toScreenX(x) + MOUSE_OFFSET
-        screenY = y + MOUSE_OFFSET + js.jQuery(".tabs-header").height()
-        x = screenX
-        if screenX + width + FLIP_DISTANCE > canvas.width():
-            x = max(0, screenX - width - FLIP_DISTANCE)
-        y = screenY
-        if screenY + height + FLIP_DISTANCE > canvas.height():
-            y = max(0, screenY - height - FLIP_DISTANCE)
-        dialog.css("left", x).css("top", y)
+        self.canvas = canvas
         self.showing = True
+        self.createDialog(html)
+        self.positionDialog(x, y)
+        self.addCloseButton()
 
     def hide(self):
-        js.jQuery("#dialog").css("display", "none")
-        js.jQuery("#hairline").css("display", "none")
         self.showing = False
+        self.dialog.css("display", "none")
+
+    def createDialog(self, html):
+        (self.dialog
+            .draggable()
+            .resizable()
+            .html(html)
+            .addClass("ui-widget-content")
+            .css("display", "block")
+            .css("max-height", js.jQuery("body").height() - 100))
+    
+    def addCloseButton(self):
+        self.dialog.append((js.jQuery("<div>")
+            .addClass("dialog-close-button")
+            .text("X")
+            .click(pyodide.ffi.create_proxy(lambda event: self.hide()))
+        ))
+    
+    def positionDialog(self, x, y):
+        width = self.dialog.width()
+        height = self.dialog.height()
+        if not js.parseFloat(self.dialog.css("left")):
+            screenX = self.canvas.toScreenX(x) + MOUSE_OFFSET
+            screenY = y + MOUSE_OFFSET + js.jQuery(".tabs-header").height()
+            x = screenX
+            if screenX + width + FLIP_DISTANCE > self.canvas.width():
+                x = max(0, screenX - width - FLIP_DISTANCE)
+            y = screenY
+            if screenY + height + FLIP_DISTANCE > self.canvas.height():
+                y = max(0, screenY - height - FLIP_DISTANCE)
+            self.dialog.css("left", x).css("top", y)
 
 dialog = Dialog()
