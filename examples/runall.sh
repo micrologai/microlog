398672990c14be0efb15ff92676d7aa7fdc60951
laffra <laffra@gmail.com>
2023-08-01 11:55:02 +0200

    Less noise

--- examples/runall.sh
+++ examples/runall.sh
@@ -1,16 +1,16 @@
 rm -rf ~/microlog/logs/*     
 
-python3 examples/binaryTrees.py&
-python3 examples/bookstore.py&
-python3 examples/example.py&
-python3 examples/go.py&
-python3 examples/helloworld.py&
-python3 examples/memory.py&
-python3 examples/files.py&
-python3 examples/modules.py&
-python3 examples/dataframes.py&
-python3 examples/threads.py&
-python3 examples/startstop.py&
+python3 examples/binaryTrees.py 2>&1 | grep "📈 Microlog" &
+python3 examples/bookstore.py 2>&1 | grep "📈 Microlog" &
+python3 examples/example.py 2>&1 | grep "📈 Microlog" &
+python3 examples/go.py 2>&1 | grep "📈 Microlog" &
+python3 examples/helloworld.py 2>&1 | grep "📈 Microlog" &
+python3 examples/memory.py 2>&1 | grep "📈 Microlog" &
+python3 examples/files.py 2>&1 | grep "📈 Microlog" &
+python3 examples/modules.py 2>&1 | grep "📈 Microlog" &
+python3 examples/dataframes.py 2>&1 | grep "📈 Microlog" &
+python3 examples/threads.py 2>&1 | grep "📈 Microlog" &
+python3 examples/startstop.py 2>&1 | grep "📈 Microlog" &
 
 wait
 wc  ~/microlog/logs/*/*.zip | sort -k 3         
